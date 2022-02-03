beb9e3dcd05ae7dc75ac712bf35761d3cebbe001
Abdirahman <dubowhamaan@gmail.com>
2022-02-03 13:51:42 +0300

    adding manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
+
+from app.models import User,Pitch,Upvote,Downvote,Comment
 
 # Creating app instance
 app = create_app('develoment')
 
 manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
