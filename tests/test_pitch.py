2d926fb284583905f4486d792a706aaaa5e006b2
Abdirahman <dubowhamaan@gmail.com>
2022-02-03 15:22:39 +0300

    updatng test_pitch.py

--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -1,22 +1,23 @@
-from app.models import User,Comment
+from app.models import Pitch,User
 from app import db
 import unittest
 
-class CommentTest(unittest.TestCase):
+class PitchTest(unittest.TestCase):
 
     def setUp(self):
         self.user_Maria = User(username = 'maria',password = 'test', email = 'maria@gg.com')
-        self.new_comment = Comment("Nice pitch",pitch_id = 123, user_id=self.user_Maria)
-        
+        self.new_pitch = Pitch(title="Money",post= "Advertise good products",category="Product",user = self.user_Maria )
+            
     def tearDown(self):
-        Comment.query.delete()
+        Pitch.query.delete()
         User.query.delete()
-        
+            
     def test_check_instance_variables(self):
-        self.assertEquals(self.new_comment.comment,"Nice pitch")
-        self.assertEquals(self.new_comment.pitch_id,123)
-        self.assertEquals(self.new_comment.user,self.user_Maria)
+        self.assertEquals(self.new_pitch.title,"Money")
+        self.assertEquals(self.new_pitch.post,"Advertise good products")
+        self.assertEquals(self.new_pitch.category,"Product")
+        self.assertEquals(self.new_pitch.user,self.user_Maria)
         
-    def test_save_comment(self):
-        self.new_comment.save_comment()
-        self.assertTrue(len(Comment.query.all())>0)
+    def test_save_pitch(self):
+        self.new_pitch.save_review()
+        self.assertTrue(len(Pitch.query.all())>0)
